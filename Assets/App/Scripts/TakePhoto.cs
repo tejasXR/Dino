7f60608af4cc1a1bad9cba68338b1a0b7f6384d2
Tejas <tejasshroff24@gmail.com>
2023-03-06 23:00:24 -0500

    Adding photo data and assigning points to photos taken

--- Assets/App/Scripts/TakePhoto.cs
+++ Assets/App/Scripts/TakePhoto.cs
@@ -1,10 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+
+public class PhotoMetaData
+{
+    public int Points;
+}
+
+[System.Serializable]
+public struct PhotoData
+{
+    public Sprite PhotoSprite;
+    public PhotoMetaData PhotoMetaData;
+
+    public PhotoData(Sprite sprite, PhotoMetaData metaData)
+    {
+        PhotoSprite = sprite;
+        PhotoMetaData = metaData;
+    }
+    
+}
 
 public class TakePhoto : MonoBehaviour
 {
     [SerializeField] private Camera photoCamera;
     [SerializeField] private Image previewImage;
+
+    private List<PhotoData> _photos = new();
+    
+    private const int PhotoWidth = 300;
+    private const int PhotoHeight = 300;
+    private const int PhotoZDepth = 10;
     
     void Update()
     {
@@ -16,11 +43,8 @@ public class TakePhoto : MonoBehaviour
 
     private void SavePhoto()
     {
-        var width = 300;
-        var height = 300;
-            
-        var rt = new RenderTexture(width, height, 24);
-        var snapshotTexture = new Texture2D(width, height, TextureFormat.RGB24, false);
+        var rt = new RenderTexture(PhotoWidth, PhotoHeight, 24);
+        var snapshotTexture = new Texture2D(PhotoWidth, PhotoHeight, TextureFormat.RGB24, false);
         
         // Setup our render texture and assign this texture to our snapshot camera 
         photoCamera.targetTexture = rt;
@@ -28,15 +52,43 @@ public class TakePhoto : MonoBehaviour
         // Render our texture and read screen pixels
         RenderTexture.active = rt;
         photoCamera.Render();
-        snapshotTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        snapshotTexture.ReadPixels(new Rect(0, 0, PhotoWidth, PhotoHeight), 0, 0);
         snapshotTexture.Apply();
         
+        var photoMetaData = GetMetaDataFromPhoto();
+
         // We've saved our texture, null out our camera render texture and destroy our cached render texture
         photoCamera.targetTexture = null;
         RenderTexture.active = null;
         Destroy(rt);
 
-        Sprite tempSprite = Sprite.Create(snapshotTexture, new Rect(0, 0, width, height), new Vector2(0, 0));
+        Sprite tempSprite = Sprite.Create(snapshotTexture, new Rect(0, 0, PhotoWidth, PhotoHeight), new Vector2(0, 0));
         previewImage.sprite = tempSprite;
+
+        var photoData = new PhotoData(tempSprite, photoMetaData);
+        _photos.Add(photoData);
+        
+        Debug.Log($"Points earned from photo: {photoMetaData.Points}");
+    }
+
+    private PhotoMetaData GetMetaDataFromPhoto()
+    {
+        RaycastHit[] hitResults = new RaycastHit[100];
+        Physics.BoxCastNonAlloc(photoCamera.transform.position,
+            new Vector3(.75F, .75F, PhotoZDepth), photoCamera.transform.forward, hitResults);
+
+        var points = 0;
+        foreach (var raycastHit in hitResults)
+        {
+            if (raycastHit.collider == null) continue;
+            if (raycastHit.collider.CompareTag("ObjectOfInterest")) points++;
+        }
+
+        var pMD = new PhotoMetaData
+        {
+            Points = points
+        };
+
+        return pMD;
     }
 }
